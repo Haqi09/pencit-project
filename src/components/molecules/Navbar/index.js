c066587e716a33ea94d04291c33b71d788023c6f
Ammal Haqi <haqi.ammal23@gmail.com>
2024-10-21 02:55:15 +0700

    Add navbar

---
+++ src/components/molecules/Navbar/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import './navbar.css';
+import logo from '../../../assets/image/logo_auth.png';
+import { House, Image, UserFocus } from 'phosphor-react';
+
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+
+        <div className='logo-nav'>
+            <img className="logo-pencit-nav" src={logo} alt="Pencit Logo" />
+            <p className="pencit-nav">Pencit</p>
+        </div>
+
+        <div className='menu'>
+            <ul>
+                <li>
+                    <a href="/">
+                        <House className="house-icon" size={40} weight="fill"/>Home
+                    </a>
+                </li>
+                <li>
+                    <a href="/create">
+                        <Image className='image-icon' size={40} weight="fill" /> Create
+                    </a>
+                </li>
+                <li>
+                    <a href="/profile">
+                        <UserFocus className="user-icon" size={40} weight="fill" />Profile
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
